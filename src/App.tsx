3cdf5475a75d2057cc7fd6b3326e29837094a2c2
fschiro <francesca.schiro@ext.grupposandonato.it>
2023-01-16 22:35:36 +0100

    [WIP] ADD icons and tabs

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { FC, FormEvent, useCallback, useEffect, useState } from 'react';
 import './App.scss';
 import { IWeatherResponse } from './api/models/WeatherResponse';
 import { getWeatherByCity, IWeatherResponseError } from './services/weather.service';
-import { getCityByZipcode, IZipCodeError } from './services/zipcode.service';
+import { getCityByZipcodeAndCountry, getGeoByZipcode, IZipCodeError } from './services/zipcode.service';
 import { COUNTRIES } from './constants';
 import { CityByZipcodeSuccessResponse } from './api/models/CityByZipcodeResponse';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { IWeatherDay } from './api/models/WeatherDay';
+// https://fontawesome.com/icons/cloud?s=light&f=classic
 
 
 const App: FC = () => {
   const [city, setCity] = useState("");
+  const [selectedCity, setSelectedCity] = useState(""); // mi serve per stampare la città derivata da zipcode e country nei risultati
   const [zipCode, setZipCode] = useState("");
   const [countryCode, setCountryCode] = useState("");
   const [weather, setWeather] = useState<IWeatherResponse | null>(null)
   const [noResultsFound, setNoResultsFound] = useState(false);
+  const [activeTab, setActiveTab] = useState<"today" | "3days">("today");
 
   useEffect(() => {
     console.log('%cApp.tsx line:17 weather', 'color: #007acc;', weather);
@@ -21,18 +26,18 @@ const App: FC = () => {
 
   const handleCityChange = useCallback(async (value: string) => {
     setCity(value);
-    if(noResultsFound) setNoResultsFound(false)
+    if (noResultsFound) setNoResultsFound(false)
   }, [noResultsFound]);
 
 
   const handleCountryCodeChange = useCallback(async (value: keyof typeof COUNTRIES | "") => {
     setCountryCode(value);
-    if(noResultsFound) setNoResultsFound(false)
+    if (noResultsFound) setNoResultsFound(false)
   }, [noResultsFound]);
 
   const handleZipCodeChange = useCallback(async (value: string) => {
     setZipCode(value);
-    if(noResultsFound) setNoResultsFound(false)
+    if (noResultsFound) setNoResultsFound(false)
   }, [noResultsFound]);
 
 
@@ -43,24 +48,26 @@ const App: FC = () => {
     let selectedCity = city;
 
     if (zipCode) {
-      const cityFromZipcodeRes = await getCityByZipcode(zipCode, countryCode);
+      await getGeoByZipcode(zipCode);
+      const cityFromZipcodeRes = await getCityByZipcodeAndCountry(zipCode, countryCode);
       if (cityFromZipcodeRes.isError) {
         throw new Error(`NO RESULTS FOUND - ZIPCODE invalid for picked country - selected city not found: ${selectedCity}`)
       }
       const successData = cityFromZipcodeRes.data as CityByZipcodeSuccessResponse;
-      const cityResult = successData.results[successData.query.codes[0]] ;
+      const cityResult = successData.results[successData.query.codes[0]];
       selectedCity = cityResult?.[0]?.city
     };
-    if(!selectedCity)  {
+    if (!selectedCity) {
       setNoResultsFound(true)
       throw new Error(`NO RESULTS FOUND - ZIPCODE invalid for picked country - selected city not found: ${selectedCity}`)
     };
+    setSelectedCity(selectedCity)
     const weatherRes = await getWeatherByCity({ city: selectedCity });
     if (weatherRes.isError) {
       setNoResultsFound(true)
       throw new Error(weatherRes.data as IWeatherResponseError["error"])
     }
-    if(!(weatherRes.data as IWeatherResponse).description) {
+    if (!(weatherRes.data as IWeatherResponse).description) {
       setNoResultsFound(true)
       throw new Error(`NO RESULTS FOUND - CITY not found: ${selectedCity}`)
     }
@@ -112,11 +119,11 @@ const App: FC = () => {
                 onChange={(event) => handleCountryCodeChange((event.target.value as "" | keyof typeof COUNTRIES))}
                 placeholder='Seleziona un paese'
                 value={countryCode}
-                >
+              >
                 <option value="">Select a country</option>
                 {Object.keys(COUNTRIES).map(countryCode => (
                   <option key={countryCode} value={countryCode}>{countryCode} - {COUNTRIES[countryCode as keyof typeof COUNTRIES]}</option>
-                  ))}
+                ))}
               </select>
 
               <input
@@ -124,7 +131,7 @@ const App: FC = () => {
                 type="text"
                 name="zipcode"
                 value={zipCode}
-                onChange={(event) => handleZipCodeChange(event.target.value)}
+                onChange={(event) => handleZipCodeChange(event.target.value.trim())}
                 placeholder="Insert zipcode"
               />
 
@@ -136,24 +143,52 @@ const App: FC = () => {
         </form>
 
         <button onClick={handleResetAll} disabled={!zipCode && !city}>Remove filters</button>
-       
+
       </div>
 
       <div className='content column'>
 
-        <div>{city}</div>
+        <div>{city ?? selectedCity}</div>
         <div>{countryCode}</div>
         <div>{zipCode}</div>
         {weather ? (
           <>
-            <div>{weather.description}</div>
-            <div>{weather.temperature}</div>
-            <div>{weather.wind}</div>
+            <div className='tabs row'>
+              <div
+                className={`tab column ${activeTab === "today" ? "active" : ""}`}
+                onClick={() => setActiveTab("today")}
+              >Today<FontAwesomeIcon icon={faCoffee} /></div>
+              <div
+                className={`tab column ${activeTab === "3days" ? "active" : ""}`}
+                onClick={() => setActiveTab("3days")}
+              >3 days</div>
+            </div>
+
+            <div className="weather-info__wrapper">
+              {activeTab === "today" ? (
+                <div className="weather-info__section">
+                  <div>{weather.description}</div>
+                  <div>{weather.temperature}</div>
+                  <div>{weather.wind}</div>
+                </div>
+              ) : weather.forecast.map((dayWeather: IWeatherDay) => (
+                <div className="weather-info__section">
+                  <div>{dayWeather.day}</div>
+                  <div>{dayWeather.temperature}</div>
+                  <div>{dayWeather.wind}</div>
+                </div>
+              ))}
+
+
+            </div>
+
+
+
           </>
         ) : null}
 
         {noResultsFound && <div className='no-results'> Sorry, no city found</div>}
-        
+
       </div>
 
     </div >
--- src/services/zipcode.service.ts
+++ src/services/zipcode.service.ts
@@ -1,4 +1,4 @@
-import { AxiosError, isAxiosError } from "axios";
+import axios, { AxiosError, isAxiosError } from "axios";
 import { ZipCodeApi } from "../api/axiosClient";
 import { CityByZipcodeSuccessResponse } from "../api/models/CityByZipcodeResponse";
 
@@ -10,12 +10,36 @@ export interface IZipCodeError {
 }
 
 //TODO: make searchQueryFromObj
-export const getCityByZipcode = async (zipCode: string, countryCode?: string) => {
+export const getCityByZipcodeAndCountry = async (zipCode: string, countryCode?: string) => {
     try {
         const response = await ZipCodeApi.get<CityByZipcodeSuccessResponse>(`/search?apikey=${ZIPCODE_API_KEY}&codes=${zipCode}${countryCode ? `&country=${countryCode}` : ``}`)
-       return { isError: false, data: response.data as CityByZipcodeSuccessResponse }
+        return { isError: false, data: response.data as CityByZipcodeSuccessResponse }
     } catch (err) {
         const errors = err as Error | AxiosError;
         return { isError: true, data: !isAxiosError(errors) ? errors.message : (errors.response?.data as IZipCodeError).error }
     }
 };
+
+// interface GeoInfo {
+//     country: string,
+//     state: string,
+//     city: string
+// };
+
+// export type GeoByZipcodeSuccessResponse = {
+//     query: {
+//         zipCode: string,
+//     },
+//     results: GeoInfo;
+// };
+
+export const getGeoByZipcode = async (zipCode: string) => {
+    try {
+        const response = await axios.get(`https://ziptasticapi.com/${zipCode}`)
+        console.log('%czipcode.service.ts line:39 response', 'color: #007acc;', response);
+    } catch (err) {
+        console.log('%czipcode.service.ts line:41 err', 'color: #007acc;', err);
+        // const errors = err as Error | AxiosError;
+//         return { isError: true, data: !isAxiosError(errors) ? errors.message : (errors.response?.data as IZipCodeError).error }
+    }
+}
